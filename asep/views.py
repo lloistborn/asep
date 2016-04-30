41d64ef8c6f59de4c4f24b1ce6640c4e1deae370
lloistborn <faisalmorensya@gmail.com>
2016-04-30 15:47:56 +0700

    add assets

--- asep/views.py
+++ asep/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+	title_page = 'Home'
+
+	return render(request, 'asep/index.html', {
+			'title_page' :	title_page,
+		})
