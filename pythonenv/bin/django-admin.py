02cc0db74792d11faf5e24ea1468be9ea6b18400
Reza Dwi Saputra <rezadwi10@reza.saputra>
2016-04-30 19:55:53 +0700

    add pythonenv and edit requirement

---
+++ pythonenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/rezadwi10/Project/Python/kudohackathon/asep/pythonenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
